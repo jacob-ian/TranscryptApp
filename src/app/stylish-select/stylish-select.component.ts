7910b22f9e30b852f787a346e375cb291eb37873
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-11-10 15:38:04 +0800

    fix(home): Created a stylish select component.

---
+++ src/app/stylish-select/stylish-select.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-stylish-select',
+  templateUrl: './stylish-select.component.html',
+  styleUrls: ['./stylish-select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => StylishSelectComponent),
+      multi: true,
+    },
+  ],
+})
+export class StylishSelectComponent implements OnInit, ControlValueAccessor {
+  // The underlying select element
+  select: HTMLInputElement;
+
+  // The data-label attribute
+  @Input('label') label: string;
+
+  // The form control name attribute
+  @Input('formControlName') formControlName: string;
+
+  // The name attribute
+  @Input('name') name: string;
+
+  // The ID attribute
+  @Input('id') id: string;
+
+  // The disabled attribute
+  @Input('disabled') disabled = false;
+
+  constructor() {}
+
+  // The select's value
+  get value(): any {
+    return this.select.value;
+  }
+
+  // The methods for change and touched
+  onChange: any = () => {};
+
+  onTouched: any = () => {};
+
+  writeValue(value: any): void {
+    // Change the value
+    this.select.value = value;
+  }
+  registerOnChange(fn: (value: any) => any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    // Set the disabled variable
+    this.disabled = isDisabled;
+  }
+
+  /**
+   * The (input) called event
+   * @param event the select element's called event
+   */
+  inputChange(event: any) {
+    // Get the event target
+    const target = event.target;
+
+    // Get the value
+    const value = target.value;
+
+    // Get the parent container (the sylish-select-wrapper)
+    const wrapper = target.parentNode;
+
+    // Check if the value is non-null
+    if (value !== '') {
+      // Add the class 'notempty' to the select wrapper
+      wrapper.classList.add('not-empty');
+    } else {
+      // Remove the 'notempty' class from the select wrapper
+      wrapper.classList.remove('not-empty');
+    }
+  }
+
+  ngOnInit(): void {
+    // Get the underlying select element
+    this.select = <HTMLInputElement>(
+      document.getElementsByClassName('stylish-select')[0]
+    );
+  }
+}
