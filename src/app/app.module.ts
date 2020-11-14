a5aff605167702b99866d7aa000448ac3e97414b
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-11-14 17:06:50 +0800

    fix(*): Remove excess modules. - Remove FlexLayoutModule - Remove ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireFunctionsModule } from '@angular/fire/functions';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +13,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { StylishSelectComponent } from './stylish-select/stylish-select.component';
+import { StylishUrlComponent } from './stylish-url/stylish-url.component';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 import { TranscriptComponent } from './transcript/transcript.component';
@@ -22,7 +22,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
-import { StylishUrlComponent } from './stylish-url/stylish-url.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -42,16 +42,14 @@ import { StylishUrlComponent } from './stylish-url/stylish-url.component';
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
-    ReactiveFormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireFunctionsModule,
-    FlexLayoutModule,
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
     MatRadioModule,
+    MatCheckboxModule,
     FormsModule,
-    ReactiveFormsModule,
   ],
   exports: [],
   providers: [],
