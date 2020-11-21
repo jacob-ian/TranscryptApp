f5d1f2d65ecbf55333813e32e6f76a4509e830d5
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-11-21 15:23:47 +0800

    feat(Footer): Add donation component
    - Add a 'Donate' link that opens the Donate component
    - Create a open-close mechanism that communicates with donate component
    - Add open-close animations for Donate component
    - Add method to close Donation component on window popstate

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  animations: [
+    trigger('showDonation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.3s ease-in-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('0.3s ease-in-out', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class FooterComponent implements OnInit {
-  @Input()
-  year: number;
+  @Input() year: number;
 
   @Input() version: string;
+
+  @Input() showDonate: boolean = false;
 
   constructor() {}
 
@@ -20,5 +34,13 @@ export class FooterComponent implements OnInit {
 
     // Get and set the version
     this.version = environment.appVersion;
+  }
+
+  /**
+   * Close the donation prompt on back button
+   */
+  @HostListener('window:popstate')
+  onPopState() {
+    this.showDonate = false;
   }
 }
