bfd2c1c3324903551fef12f131147b457853ad57
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-11-21 15:17:00 +0800

    feat(*): Add Stripe payments.
    - Add Stripe dependencies
    - Add Stripe keys to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+/**
+ * Create variables for the production environment.
+ */
+
+// Import the version from package.json
+import { version } from '../../package.json';
+
+// Setup the environment variables
 export const environment = {
-  production: true
+  production: true,
+  appVersion: version,
+  stripe: {
+    publishableKey:
+      'pk_live_51HhpYRKhhuCbc2FakmShlIJmRpVMAmCY9rkIlU5qJGklyCaTEo9QVocfD9cPq7ucKN4Kj70tgy4qA368UbAseU3K00PNcTeUpX',
+  },
 };
