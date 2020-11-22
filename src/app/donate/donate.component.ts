08666beb965f1c8fe7d2d2825b1e41ed6b0d4e10
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-11-22 16:12:48 +0800

    feat(Donate): Add donate by card.
    - Add new email component
    - Add Stripe card payment processing
    - Add confirmation messages to payments
    - Fix DOM issues with Stripe

--- src/app/donate/donate.component.ts
+++ src/app/donate/donate.component.ts
@@ -11,6 +11,8 @@ import { trigger, style, animate, transition } from '@angular/animations';
 
 import { TranscryptService } from '../transcrypt.service';
 import { environment } from 'src/environments/environment';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { ThemePalette } from '@angular/material/core';
 
 // Define the stripe namespace
 declare var Stripe;
@@ -46,15 +48,15 @@ export class DonateComponent implements OnInit {
   // Create an error message string
   @Input() error: string;
 
-  // Create a boolean for whether or not a payment request can be made
-  @Input() canMakePayment: boolean = false;
+  // Create a payment success thank you/confirmation message
+  @Input() thanks: string;
 
-  // Create a payment success thank you boolean;
-  @Input() thanks: boolean = false;
+  // Create an input for the customer's email address
+  @Input() customerEmail: string;
 
   // Define stripe and its elements
   stripe;
-  payButton;
+  cardElement;
 
   // Define the payment amounts
   paymentOptions = [
@@ -68,122 +70,167 @@ export class DonateComponent implements OnInit {
   paymentAmount: number;
   paymentCurrency: string = 'aud';
 
+  // The show card details boolean
+  @Input() showDetails: boolean = false;
+
+  // The payment button visibility boolean
+  @Input() showButton: boolean = false;
+
+  // The form visibility boolean
+  @Input() showForm: boolean = true;
+
+  // Create a timer for mounting the card
+  timer: any;
+
+  // Create variabies for the payment loading spinner
+  @Input() loading: boolean = false;
+  @Input() loaderMode: ProgressSpinnerMode = 'indeterminate';
+  @Input() loaderColor: ThemePalette = 'accent';
+  @Input() loaderDiameter: number = 24;
+
   /**
    * Instantiate the component
    * @param service the injectible Transcrypt Service
    */
   constructor(private service: TranscryptService) {}
 
-  ngOnInit(): void {}
-
-  /**
-   * On change of value of the donation amount, the stripe element will be created.
-   * @value the cents value of the donation
-   * @returns a Promise of a void
-   */
-  async createPaymentRequest() {
+  ngOnInit(): void {
     // Load Stripe
     this.stripe = Stripe(environment.stripe.publishableKey);
 
-    // Check to make sure that the amount is non-null
-    if (this.paymentAmount) {
-      // Get the Stripe Elements
-      const elements = this.stripe.elements();
+    // Create the stripe element
+    const elements = this.stripe.elements();
 
-      // Create the payment request
-      const paymentRequest = this.stripe.paymentRequest({
-        country: 'AU',
-        currency: this.paymentCurrency,
-        total: { label: 'Donation', amount: parseInt(`${this.paymentAmount}`) },
-        requestPayerName: true,
-        requestPayerEmail: true,
-      });
+    // Style the card element
+    const style = {
+      base: {
+        color: '#000',
+        fontFamily: 'Archivo, Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: 'rgba(0,0,0,0.5)',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    };
 
-      // Get the multi option payment request element
-      this.payButton = elements.create('paymentRequestButton', {
-        paymentRequest,
-        style: {
-          paymentRequestButton: {
-            theme: 'light',
-          },
+    // Create the card element
+    this.cardElement = elements.create('card', { style: style });
+  }
+
+  /**
+   * Mount the card element to the card-element div
+   * @param mount boolean, true if mounting required
+   */
+  mountCardElement(mount: boolean): void {
+    // Check if we are mounting or not
+    if (mount) {
+      // Set the show details boolean to true
+      this.showDetails = true;
+
+      // Check if the timeout is already made
+      if (this.timer) {
+        // Clear the timer and call again
+        clearTimeout(this.timer);
+      }
+
+      // Create a timeout
+      this.timer = setTimeout(() => {
+        // Mount the card element
+        this.cardElement.mount('#card-element');
+
+        // Regirster an on input change listener for the card element
+        this.cardElement.on('change', (event) => {
+          // Remove the error
+          this.error = null;
+
+          // Hide the button
+          this.showButton = false;
+
+          // Check if the validation is complete
+          if (event.complete) {
+            // Show the donate button
+            this.showButton = true;
+          } else if (event.error) {
+            // The card didn't validate correctly, show the error and hide the button
+            this.showButton = false;
+            this.error = event.error.message;
+          }
+        });
+      }, 100);
+    } else {
+      // Hide the card details
+      this.showDetails = false;
+    }
+  }
+
+  /**
+   * Submits the payment to the back-end handler.
+   * @param event The form submission event
+   */
+  async submitPayment(event: any): Promise<void> {
+    // Stop the default action from occurring
+    event.preventDefault();
+
+    // Call the method to pay with card
+    try {
+      return await this.payWithCard();
+    } catch (err) {
+      // Stop loading and show the error
+      this.loading = false;
+      this.error = err.message;
+    }
+  }
+
+  /**
+   * Attempt to pay with the provided card
+   */
+  async payWithCard(): Promise<void> {
+    // Start loading and hide the form
+    this.loading = true;
+
+    // Hide the form
+    this.showForm = false;
+
+    // Create a payment intent and get the Stripe Client Secret
+    try {
+      var clientSecret = await this.service.getStripeClientSecret(
+        this.paymentAmount,
+        this.paymentCurrency
+      );
+    } catch (err) {
+      // Rethrow the error
+      throw err;
+    }
+
+    // Attempt to confirm the card payment
+    try {
+      var paymentResult = await this.stripe.confirmCardPayment(clientSecret, {
+        receipt_email: this.customerEmail,
+        payment_method: {
+          card: this.cardElement,
         },
       });
+    } catch (err) {
+      // Rethrow the error
+      throw err;
+    }
 
-      // Check if we can make a payment
-      this.canMakePayment = await paymentRequest.canMakePayment();
-
-      // Check if it can make a payment
-      if (this.canMakePayment) {
-        // Mount the button
-        this.payButton.mount('#payment-button-container');
-
-        // Add on on change listener to display errors
-        this.payButton.addEventListener('change', ({ error }) => {
-          // Set the error message
-          this.error = error && error.message;
-        });
-
-        // Create a listener for the paymentRequest
-        paymentRequest.on('paymentmethod', async (ev) => {
-          // Get the client secret from the service
-          try {
-            var clientSecret = this.service.getStripeClientSecret(
-              this.paymentAmount,
-              this.paymentCurrency
-            );
-          } catch (err) {
-            // Return the function and set the error message
-            return (this.error = err.message);
-          }
-
-          // Confirm the paymentIntent
-          const {
-            paymentIntent,
-            error: confirmError,
-          } = await this.stripe.confirmCardPayment(
-            clientSecret,
-            { payment_method: ev.paymentMethod.id },
-            { handleActions: false }
-          );
-
-          // Check if the payment failed
-          if (confirmError) {
-            // Show an error
-            this.error = 'The donation payment failed.';
-            ev.complete('fail');
-          } else {
-            // The payment confirmation was successful, we can process the payment
-            ev.complete('success');
-
-            // Check the payment intent
-            if (paymentIntent.status === 'requires_action') {
-              // Let Stripe handle the rest
-              const { error } = await this.stripe.confirmCardPayment(
-                clientSecret
-              );
-
-              // Check for an error
-              if (error) {
-                // The payment failed, return an error
-                this.error =
-                  'The donation payment failed, please try another payment method.';
-              } else {
-                // The payment was successful
-                this.thanks = true;
-              }
-            } else {
-              // The payment was successful
-              this.thanks = true;
-            }
-          }
-        });
-      } else {
-        // Return an error
-        this.error = "Sorry, we couldn't create a Stripe payment request.";
-      }
+    // Check if there was an error in the result
+    if (paymentResult.error) {
+      // Throw an error with the message
+      throw { message: paymentResult.error.message };
     } else {
-      // Get rid of the payButton element
-      this.payButton = null;
+      // The payment was successful, display a success message
+      // Stop the loading
+      this.loading = false;
+
+      // Display the success message
+      this.thanks = `<h3>Donation Successful!</h3><p>Confirmation ID:</p><p class="donation-confirmation">${paymentResult.paymentIntent.id}</p><br><p>Thank you for your donation!</p>`;
     }
   }
 
@@ -192,10 +239,13 @@ export class DonateComponent implements OnInit {
    * @param value true for show, false for hide
    */
   setShow(value: boolean): void {
-    // Change the value
-    this.show = value;
+    // Prevent the dialog from closing if the payment is processing
+    if (!this.loading) {
+      // Change the value
+      this.show = value;
 
-    // Emit the value to the parent component
-    this.showChanged.emit(value);
+      // Emit the value to the parent component
+      this.showChanged.emit(value);
+    }
   }
 }
