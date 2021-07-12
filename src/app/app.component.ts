72e0f91260acf2f990a9916da1338ff66388acd3
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-07-13 08:27:36 +1000

    fix(index): Add Open Graph meta info
    - Add og-image to assets
    - Add og:image, og:description, og:title to index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,5 @@ import { clear, time } from 'console';
 export class AppComponent implements OnInit {
   title = 'TranscriptApp';
 
-  ngOnInit(): void {
-    /*
-    // Set the viewport wrapper to the window height
-    const wrapper = document.getElementsByClassName('wrapper')[0];
-    wrapper.setAttribute('style', `min-height:${window.innerHeight}px`);
-
-    // Create a timeout for the resize window delay
-    var timeoutId;
-    var delay = 300; // 200ms
-
-    // Make sure to update the height of the viewport on resizing
-    window.addEventListener('resize', () => {
-      // Clear the timeout
-      clearTimeout(timeoutId);
-
-      // Delay the resize function
-      timeoutId = setTimeout(() => {
-        // Set the new height
-        wrapper.setAttribute('style', `min-height:${window.innerHeight}px`);
-      }, delay);
-    });
-    */
-  }
+  ngOnInit(): void {}
 }
